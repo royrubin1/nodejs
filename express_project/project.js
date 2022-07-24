4953fce6d5b23d648a587b991783bb75e857c8dc
Roy Rubin <royrubin@protonmail.com>
2022-07-24 18:27:49 +0200

    Chapter 65

--- express_project/project.js
+++ express_project/project.js
@@ -1,18 +1,20 @@
 const http = require('http');
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({extended: false}));
 
 app.use('/add-product',(req, res, next) => {
-    console.log('in another middleware!');
-    res.send(<form action="/product" method="POST" ><input type="text" name="title" ></input><button type="submit">Add Product</button></form>);
+    res.send('<form action="/product" method="POST" ><input type="text" name="title" ></input><button type="submit">Add Product</button></form>');
 });
 
-app.use('/product', (req, res, next) => {
-    res.redirect()
+app.post('/product', (req, res, next) => {
+    console.log(req.body);
+    res.redirect('/')
 });
 
 app.use('/',(req, res, next) => {
-    console.log('in another middleware!');
     res.send('<h1>Main Page</h1>');
 });
 
