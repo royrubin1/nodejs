5cbe3872d5c6871bbf1f2a645c2955f177c7df3a
Roy Rubin <royrubin@protonmail.com>
2022-07-24 19:29:47 +0200

    Chapter 66

--- express_project/project.js
+++ express_project/project.js
@@ -2,22 +2,12 @@ const http = require('http');
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const adminRoutes = require('./routes/admin');5
+const shopRoutes = require('./routes/shop')
+const server = http.createServer(app);
 
 app.use(bodyParser.urlencoded({extended: false}));
+app.use(adminRoutes);
+app.use(shopRoutes);
 
-app.use('/add-product',(req, res, next) => {
-    res.send('<form action="/product" method="POST" ><input type="text" name="title" ></input><button type="submit">Add Product</button></form>');
-});
-
-app.post('/product', (req, res, next) => {
-    console.log(req.body);
-    res.redirect('/')
-});
-
-app.use('/',(req, res, next) => {
-    res.send('<h1>Main Page</h1>');
-});
-
-
-const server = http.createServer(app);
 server.listen(3000);
---
+++ express_project/routes/admin.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const router = express.Router();
+
+
+
+router.get('/add-product',(req, res, next) => {
+    res.send('<form action="/product" method="POST" ><input type="text" name="title" ></input><button type="submit">Add Product</button></form>');
+});
+
+router.post('/product', (req, res, next) => {
+    console.log(req.body);
+    res.redirect('/')
+});
+
+router.use('/',(req, res, next) => {
+    res.send('<h1>Main Page</h1>');
+});
+
+module.exports = router
