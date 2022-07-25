a61f97f75165b340e54a390dd39795d79513f1b0
Roy Rubin <royrubin@protonmail.com>
2022-07-26 01:19:55 +0200

    Chapter 75

--- express_project/project.js
+++ express_project/project.js
@@ -9,13 +9,14 @@ const path = require('path');
 const rootDir = require('./util/path')
 
 app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.use('/admin',adminRoutes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
     //res.status(404).send('<h1>Page not found</h1>')
-    res.status(404).sendFile(path.join(rootDir,'views', 'err404.html'));
+    res.status(404).sendFile(path.join(rootDir,'views', '404.html'));
 });
 
 server.listen(3000);
