15c6a1ae813cb580e3af50e9890b44a4931d8622
Roy Rubin <royrubin@protonmail.com>
2022-07-25 19:37:37 +0200

    Chapter 71

--- express_project/project.js
+++ express_project/project.js
@@ -5,6 +5,7 @@ const bodyParser = require('body-parser');
 const adminRoutes = require('./routes/admin');5
 const shopRoutes = require('./routes/shop')
 const server = http.createServer(app);
+const path = require('path');
 
 app.use(bodyParser.urlencoded({extended: false}));
 
@@ -12,7 +13,8 @@ app.use('/admin',adminRoutes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).send('<h1>Page not found</h1>')
+    //res.status(404).send('<h1>Page not found</h1>')
+    res.status(404).sendFile(path.join(__dirname,'views', 'err404.html'));
 });
 
 server.listen(3000);
