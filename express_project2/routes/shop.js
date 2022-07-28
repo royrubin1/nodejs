5a69e7f0dde1d00b0f5ad7d76f3873855310a902
Roy Rubin <royrubin@protonmail.com>
2022-07-29 01:38:43 +0200

    Chapter 81

--- express_project2/routes/shop.js
+++ express_project2/routes/shop.js
@@ -9,7 +9,8 @@ const adminData = require('./admin')
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-  res.sendFile(path.join(rootDir, 'views', 'shop.html'));
+  //res.sendFile(path.join(rootDir, 'views', 'shop.html'));
+  res.render('shop');
 });
 
 module.exports = router;
