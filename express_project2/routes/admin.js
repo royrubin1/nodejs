e7ac58000dabc5b94d1688b23bd2f854285d55cd
Roy Rubin <royrubin@protonmail.com>
2022-07-31 23:48:02 +0200

    Chapter 90

--- express_project2/routes/admin.js
+++ express_project2/routes/admin.js
@@ -11,7 +11,12 @@ const products = [];
 // /admin/add-product => GET
 router.get('/add-product', (req, res, next) => {
   //res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
-  res.render('add-product', {pageTitle: 'Add Product', path: '/admin/add-product'})
+  res.render('add-product', {
+    pageTitle: 'Add Product', 
+    path: '/admin/add-product', 
+    formCSS: true, 
+    productCSS: true, 
+    activeAddProduct: true})
 });
 
 // /admin/add-product => POST
